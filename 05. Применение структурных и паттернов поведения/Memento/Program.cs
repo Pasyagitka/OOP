61711690b2675bc7ed42e4e4f58b2992bd31ce09
Lizaveta Zinovich <lizavetazinovich@gmail.com>
2021-03-20 23:51:34 +0300

    Memento

---
+++ 05. Применение структурных и паттернов поведения/Memento/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            Flat flat = new Flat();
+            flat.Price = 11;
+            PriceHistory price = new PriceHistory();
+            price.History.Push(flat.SaveState());
+            flat.Price = 17;
+            price.History.Push(flat.SaveState());
+            flat.Price = 19;
+            Console.WriteLine(flat.Price + " - Текущая цена квартиры");
+            flat.RestoreState(price.History.Pop());
+            flat.RestoreState(price.History.Pop());
+        }
+    }
+}
