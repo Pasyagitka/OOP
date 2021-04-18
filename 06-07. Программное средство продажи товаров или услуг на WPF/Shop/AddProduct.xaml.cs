7143889d3ecf4b95b4d90dfffb2970377e7f3e2f
pasyagitka <lizavetazinovich@gmail.com>
2021-04-19 01:47:41 +0300

    Images

---
+++ 06-07. Программное средство продажи товаров или услуг на WPF/Shop/AddProduct.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Shop.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Shop.Command;
+
+namespace Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        private Invoker invoker;
+        public AddProduct()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            //
+            Sweetness sw = SweetsFactory.CreateSweet((Sweets)typeCB.SelectedIndex, nameTB.Text, imageTB.Text, Convert.ToDouble(weightTB.Text), 
+                Convert.ToInt32(caloriesTB.Text), descriptionTB.Text, trademarkTB.Text, Convert.ToInt32(priceTB.Text));
+            
+            invoker ??= new Invoker();
+            var command = new AddCommand(sw);
+            invoker.SetCommand(command);
+            invoker.Run();
+            this.Close();
+        }
+    }
+}
---
+++ 06-07. Программное средство продажи товаров или услуг на WPF/Shop/Controls/ProductContainer.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Controls;
+using Shop.Product;
+
+namespace Shop.Controls
+{
+    public partial class ProductContainer : UserControl
+    {
+        public ProductContainer()
+        {
+            InitializeComponent();
+            ShoppingCardsList.ItemsSource = Products.GetInstance().SweetnessesList;
+        }
+
+        public void CategoryUpdate(int filterIndex)
+        {
+            switch (filterIndex)
+            {
+                case 0:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.Where(x => x is Candy));
+                        break;
+                    }
+                case 1:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.Where(x => x is Chocolate));
+                        break;
+                    }
+                case 2:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.Where(x => x is Marmalade));
+                        break;
+                    }
+                case 3:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.Where(x => x is Waffles));
+                        break;
+                    }
+                default:
+                    {
+                        ShoppingCardsList.ItemsSource = Products.GetInstance().SweetnessesList;
+                        break;
+                    }
+            }
+        }
+
+        public void SortUpdate(int sortIndex)
+        {
+            switch (sortIndex)
+            {
+                case 0:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.OrderByDescending(x=>x.Price));
+                        break;
+                    }
+                case 1:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.OrderBy(x => x.Price));
+                        break;
+                    }
+                case 2:
+                    {
+                        ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.OrderBy(x => x.Name));
+                        break;
+                    }
+                default:
+                    {
+                        ShoppingCardsList.ItemsSource = Products.GetInstance().SweetnessesList;
+                        break;
+                    }
+            }
+        }
+
+        public void SearchUpdate(string search, int i)
+        {
+            if (i == 0)
+            {
+                ShoppingCardsList.ItemsSource = new ObservableCollection<Sweetness>(Products.GetInstance().SweetnessesList.Where(x => x.ToString().Contains(search)).ToList());
+            }
+            else
+            {
+                ShoppingCardsList.ItemsSource = Products.GetInstance().SweetnessesList;
+            }
+        }
+    }
+}
