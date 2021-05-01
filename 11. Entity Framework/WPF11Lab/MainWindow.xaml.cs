8850c2f1baa6e0b3191950ecedc4c74a394ac195
pasyagitka <lizavetazinovich@gmail.com>
2021-05-01 18:52:11 +0300

    11 db output

--- 11. Entity Framework/WPF11Lab/MainWindow.xaml.cs
+++ 11. Entity Framework/WPF11Lab/MainWindow.xaml.cs
@@ -22,18 +22,25 @@ namespace WPF11Lab
         {
             InitializeComponent();
 
+            //using (ClientContext db = new ClientContext())
+            //{
+            //    Address ad1 = new Address { Country = "Беларусь", City = "Минск", Street = "Свердлова", House = 11, PostalCode = 220044 };
+            //    Address ad2 = new Address { Country = "Беларусь", City = "Минск", Street = "Серова", House = 23, PostalCode = 220066 };
+
+
+            //    // добавляем объекты Book в контекст данных
+            //    db.Addresses.Add(ad1);
+            //    db.Addresses.Add(ad2);
+
+            //    // сохраняем контекст данных в базу данных
+            //    db.SaveChanges();
+            //}
             using (ClientContext db = new ClientContext())
             {
-                Address ad1 = new Address { Country = "Беларусь", City = "Минск", Street = "Свердлова", House = 11, PostalCode = 220044 };
-                Address ad2 = new Address { Country = "Беларусь", City = "Минск", Street = "Серова", House = 23, PostalCode = 220066 };
-
-
-                // добавляем объекты Book в контекст данных
-                db.Addresses.Add(ad1);
-                db.Addresses.Add(ad2);
-
-                // сохраняем контекст данных в базу данных
-                db.SaveChanges();
+                db.Addresses.Load();
+                db.Clients.Load();
+                AddressesGrid.ItemsSource = db.Addresses.Local.ToBindingList();
+                ClientsGrid.ItemsSource = db.Clients.Local.ToBindingList();
             }
         }
     }
